bdae35f0de8ac1b7122e7fb2e452426e313c096f
francisco <franciscovidalsantos@gmail.com>
2025-03-23 20:58:08 +0000

    adding our board widgets
    - displayed the grid with tiles in our UI.
    - implemented a get to retrive color identification for each tile given the applied conditions.

--- lib/screens/puzzle.dart
+++ lib/screens/puzzle.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:puzzle_a_day/widgets/board_grid.dart';
 
 class PuzzleScreen extends StatefulWidget {
   const PuzzleScreen({super.key});
@@ -11,6 +12,6 @@ class PuzzleScreen extends StatefulWidget {
 class _PuzzleScreenState extends State<PuzzleScreen> {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Puzzle Screen"));
+    return Center(child: BoardGrid());
   }
 }
